48c699cd012c1b14e41192a8c88a0a2d68ddc1a4
Debonet <719893090@qq.com>
2022-04-07 15:17:21 +0800

    feat: add will-change-transform to smooth the first trantision

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -26,13 +26,13 @@ const About: FC = () => {
   return (
     <div className="mx-auto flex max-w-5xl flex-col items-center pt-40">
       <div className="group relative rounded-full bg-light-outstand p-20 transition-colors dark:bg-dark-outstand">
-        <div className="relative h-44 w-44 rounded-full bg-gray-400 transition-transform duration-300 clip-profile group-hover:scale-105">
+        <div className="relative h-44 w-44 rounded-full bg-gray-400 transition-transform duration-300 will-change-transform clip-profile group-hover:scale-105">
           <img
             src="/images/profile.svg"
-            className="absolute -top-8 transition-transform duration-300 group-hover:-translate-y-6 group-hover:-rotate-2 group-hover:scale-110"
+            className="absolute -top-8 transition-transform duration-300 will-change-transform group-hover:-translate-y-6 group-hover:-rotate-2 group-hover:scale-110"
           />
         </div>
-        <div className="absolute top-0 left-0 h-84 w-84 scale-0 rounded-full bg-gray-400/20 transition-transform duration-300 ease-in group-hover:scale-100">
+        <div className="absolute top-0 left-0 h-84 w-84 scale-0 rounded-full bg-gray-400/20 transition-transform duration-300 ease-in will-change-transform group-hover:scale-100">
           <SvgLink component={<GithubSvg />} className="absolute left-36 bottom-4" url="https://github.com/Debonex" />
           <SvgLink
             component={<OsuSvg />}
