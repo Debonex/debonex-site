4519c0cb1d6b2d436fa3e2dc030bc3450dae7e8e
Debonet <719893090@qq.com>
2022-06-07 14:08:56 +0800

    refactor: remove less

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
-import "./styles/main.less"
+import "./styles/fonts.css"
+import "./styles/tailwind.css"
 
 ReactDOM.render(
   <React.StrictMode>
