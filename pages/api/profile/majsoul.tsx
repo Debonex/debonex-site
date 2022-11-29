280ff676ed8e99209c4f5c9baad6111702301a50
Debonet <719893090@qq.com>
2022-11-29 14:45:56 +0800

    wip: echarts ssr test

--- pages/api/profile/majsoul.tsx
+++ pages/api/profile/majsoul.tsx
@@ -1,4 +1,6 @@
 import { NextApiHandler } from "next";
+import { renderToString } from "react-dom/server";
+import MajsoulProfile from "components/profile/MajsoulProfile";
 
 const API_URL = "https://1.data.amae-koromo.com/api/v2";
 const PL4_URL = `${API_URL}/pl4`;
@@ -9,13 +11,23 @@ const profile: NextApiHandler = async (req, res) => {
     return res.status(405).send({ error: "Method not allowed" });
   }
 
+  return res
+    .status(200)
+    .setHeader("Content-Type", "image/svg+xml")
+    .end(renderToString(<MajsoulProfile />));
+
   const response = await fetch(
     `${PL4_URL}/player_stats/${11341035}/0/9999999999999?mode=8,9,11,12,15,16`
   );
 
   if (response.ok) {
-    const data = (await response.json()) as PlayerStats;
-    return res.status(200).json(data);
+    // const data = (await response.json()) as PlayerStats;
+    // console.log(data);
+    return res
+      .status(200)
+      .setHeader("Content-Type", "image/svg+xml")
+      .end(renderToString(<MajsoulProfile />));
+    // return res.status(200).json(data);
   }
 
   return res.status(200).json({ a: 1 });
